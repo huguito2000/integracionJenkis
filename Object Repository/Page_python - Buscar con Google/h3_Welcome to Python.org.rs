234829ecbadd5859e2dc5145e7c14c8c5c838200
Hugo <ing.hugo.olivera.88@gmail.com>
2023-03-06 13:42:14 -0600

    first commit

---
+++ Object Repository/Page_python - Buscar con Google/h3_Welcome to Python.org.rs
@@ -0,0 +1,116 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h3_Welcome to Python.org</name>
+   <tag></tag>
+   <elementGuidId>f7b35ddd-8d97-4108-baa9-6b21d7da71b5</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id=&quot;rso&quot;]/div[1]/div/div/div/div/div/div/div/div[1]/a/h3
+</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#rso > div:nth-child(1) > div > div > div > div > div > div > div > div.yuRUbf > a > h3</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>CSS</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h3</value>
+      <webElementGuid>00f01196-5b9f-4976-adf5-4c1bd7533787</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>LC20lb MBeuO DKV0Md</value>
+      <webElementGuid>fd3f3558-556c-40d1-9281-81d63a813f61</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Welcome to Python.org</value>
+      <webElementGuid>ee350825-26a9-46b3-a560-81a3461bf322</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;rso&quot;)/div[@class=&quot;hlcw0c&quot;]/div[@class=&quot;MjjYud&quot;]/div[@class=&quot;g&quot;]/div[@class=&quot;BYM4Nd&quot;]/div[@class=&quot;eKjLze&quot;]/div[@class=&quot;g&quot;]/div[1]/div[@class=&quot;tF2Cxc&quot;]/div[@class=&quot;yuRUbf&quot;]/a[1]/h3[@class=&quot;LC20lb MBeuO DKV0Md&quot;]</value>
+      <webElementGuid>85b3d5e6-9bc8-4783-9e0a-c415aa5085ae</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='rso']/div/div/div/div/div/div/div/div/div/a/h3</value>
+      <webElementGuid>962813c4-8dd0-471f-8d60-7d287537630c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Resultado web con vínculos a sitio'])[1]/following::h3[1]</value>
+      <webElementGuid>c1678a95-23a3-4831-b745-9549804d504d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Resultados de búsqueda'])[1]/following::h3[1]</value>
+      <webElementGuid>84c659b9-e1f8-4769-9da2-ddc07c107459</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='https://www.python.org'])[1]/preceding::h3[1]</value>
+      <webElementGuid>9e29677d-3f55-46bc-9509-81f3ba65247c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='https://www.python.org'])[2]/preceding::h3[1]</value>
+      <webElementGuid>2448c375-3ea9-4fe5-9940-a1eff4b41bc9</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Welcome to Python.org']/parent::*</value>
+      <webElementGuid>24c98c6e-124c-4784-ba74-f4da27aef519</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//h3</value>
+      <webElementGuid>88aabf7f-c399-423d-ae32-f57dbccf5290</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//h3[(text() = 'Welcome to Python.org' or . = 'Welcome to Python.org')]</value>
+      <webElementGuid>abad5ea6-954b-4994-9390-54d5623ddf7e</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
